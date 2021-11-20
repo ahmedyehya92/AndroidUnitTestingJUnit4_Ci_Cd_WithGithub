0bfe4d09435df2c622cc597cbcf2b9a907319dfa
Ahmed Yehya <ahmedyehya1992@gmail.com>
2021-11-21 00:47:48 +0200

    migrate to android x

--- app/src/main/java/com/journaldev/androidunittestingjunit4/MainActivity.java
+++ app/src/main/java/com/journaldev/androidunittestingjunit4/MainActivity.java
@@ -1,11 +1,12 @@
 package com.journaldev.androidunittestingjunit4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
